9ef1a4f955abe931e4c7dc47f77dddad06f57009
coderr011001 <christianjamesbalesteros@gmail.com>
2025-07-18 20:56:21 +0800

    picture changes
    changes of pictures in the login, register, and also the logo of the system

--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -1,30 +1,37 @@
-
-
 <div class="branding d-flex align-items-center">
-
   <div class="container position-relative d-flex align-items-center justify-content-between">
-    <a href="index.html" class="logo d-flex align-items-center me-auto">
-      <!-- Uncomment the line below if you also wish to use an image logo -->
-      <img src="assets/img/logo.png" alt="">
-      <h1 class="sitename">MediConnect</h1>
+    
+    <!-- Logo + Site Name -->
+    <a href="{{ url('/') }}" class="logo d-flex align-items-center me-auto">
+      <img src="{{ asset('assets/img/logo.png') }}" alt="MediConnect Logo" width="50" height="50">
+      <h1 class="sitename ms-2 mb-0">MediConnect</h1>
     </a>
 
-    <nav id="navmenu" class="navmenu">
-      <ul>
-        <li><a href="#home" class="active">Home<br></a></li>
+    <!-- Desktop Navigation -->
+    <nav id="navmenu" class="navmenu d-none d-xl-block">
+      <ul class="d-flex gap-4 mb-0">
+        <li><a href="#home" class="active">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#services">Services</a></li>
-
         <li><a href="#developers">Developers</a></li>
-
-        
       </ul>
-      <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
     </nav>
 
-    <a class="cta-btn d-none d-sm-block" href="{{ url('login') }}">Login</a>
-    <a class="cta-btn d-none d-sm-block" href="{{ url('register') }}">Register</a>
+    <!-- Auth Buttons -->
+    @if (Route::has('login'))
+    <div class="auth-buttons d-flex align-items-center gap-3">
+      @auth
+        <x-app-layout />
+      @else
+        <a class="cta-btn btn btn-primary" href="{{ route('login') }}">Login</a>
+        @if (Route::has('register'))
+          <a class="cta-btn btn btn-outline-primary" href="{{ route('register') }}">Register</a>
+        @endif
+      @endauth
+    </div>
+    @endif
 
+    <!-- Mobile Menu Toggle -->
+    <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
   </div>
-
 </div>
