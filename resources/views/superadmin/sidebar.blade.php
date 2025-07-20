33ad4456a72efdfc1239edbc52669875d0643851
coderr011001 <christianjamesbalesteros@gmail.com>
2025-07-20 20:45:53 +0800

    s
    s

--- resources/views/superadmin/sidebar.blade.php
+++ resources/views/superadmin/sidebar.blade.php
@@ -1,53 +1,60 @@
 <div class="sidebar-menu">
-                <div class="sidebar-header">
-                    <div class="logo">
-                        <a href=""
-                            ><img src="assets/img/logreglogowhite.png" alt="logo"
-                        /></a>
-                    </div>
-                </div>
-
-            <div class="main-menu">
-                <div class="menu-inner">
-                    <nav>
-                        <ul class="metismenu" id="menu">
-                            <li class="active">
-                                <a href="javascript:void(0)" aria-expanded="true"><i class="ti-dashboard"></i><span>dashboard</span></a>
-                              
-                            </li>
-                         
-                            <li>
-                                <a href="javascript:void(0)" aria-expanded="true"><i class="ti-user"></i><span>Customers</span></a>
-                                <ul class="collapse">
-                                    <li><a href="{{url('create_customer')}}">Add Customers</a></li>
-                                </ul>
-                                 <ul class="collapse">
-                                    <li><a href="barchart.html">View Customers</a></li>
-                                </ul>
-                            </li>
-
-                            <li>
-                                <a href="javascript:void(0)" aria-expanded="true"><i class="ti-shopping-cart"></i><span>Drugstores</span></a>
-                                <ul class="collapse">
-                                
-                                    <li><a href="progressbar.html">Create Drugstore</a></li>
-                                    <li><a href="tab.html">View Drugstore</a></li>
-                                    <li><a href="typography.html">View Inventory</a></li>
-                                    <li><a href="form.html">Form</a></li>
-                                    <li><a href="grid.html">grid system</a></li>
-                                </ul>
-                            </li>
-                            <li>
-                                <a href="javascript:void(0)" aria-expanded="true"><i class="ti-hummer"></i><span>Settings</span></a>
-                                <ul class="collapse">
-                                    <li><a href="fontawesome.html">fontawesome icons</a></li>
-                                    <li><a href="themify.html">themify icons</a></li>
-                                </ul>
-                            </li>
-                           
-                          
-                        </ul>
-                    </nav>
-                </div>
-            </div>
+    <div class="sidebar-header">
+        <div class="logo">
+            <a href="{{ route('home') }}">
+                <img src="{{ asset('assets/img/logreglogowhite.png') }}" alt="logo" />
+            </a>
         </div>
+    </div>
+
+    <div class="main-menu">
+        <div class="menu-inner">
+            <nav>
+                <ul class="metismenu" id="menu">
+                    <li class="active">
+                        <a href="{{ route('home') }}" aria-expanded="true">
+                            <i class="ti-dashboard"></i>
+                            <span>Dashboard</span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="javascript:void(0)" aria-expanded="false">
+                            <i class="ti-user"></i>
+                            <span>Customers</span>
+                        </a>
+                        <ul class="collapse">
+                            <li><a href="{{ route('superadmin.create_customer') }}">Add Customers</a></li>
+                            <li><a href="#">View Customers</a></li> <!-- Replace with actual route -->
+                        </ul>
+                    </li>
+
+                    <li>
+                        <a href="javascript:void(0)" aria-expanded="false">
+                            <i class="ti-shopping-cart"></i>
+                            <span>Drugstores</span>
+                        </a>
+                        <ul class="collapse">
+                            <li><a href="#">Create Drugstore</a></li> <!-- Replace with actual route -->
+                            <li><a href="#">View Drugstore</a></li>   <!-- Replace with actual route -->
+                            <li><a href="#">View Inventory</a></li>   <!-- Replace with actual route -->
+                            <li><a href="#">Form</a></li>             <!-- Replace with actual route -->
+                            <li><a href="#">Grid System</a></li>      <!-- Replace with actual route -->
+                        </ul>
+                    </li>
+
+                    <li>
+                        <a href="javascript:void(0)" aria-expanded="false">
+                            <i class="ti-hummer"></i>
+                            <span>Settings</span>
+                        </a>
+                        <ul class="collapse">
+                            <li><a href="#">FontAwesome Icons</a></li> <!-- Replace with actual route -->
+                            <li><a href="#">Themify Icons</a></li>      <!-- Replace with actual route -->
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -6,16 +6,15 @@ use App\Http\Controllers\SuperadminController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\DrugstoreController;
 
-//Landing Page Routes
-Route::get('/',[LandingController::class,'home']);
+// Landing Page Routes
+Route::get('/', [LandingController::class, 'home']);
 
-
-//Superadmin Routes
+// Superadmin Routes
 Route::get('/home', [SuperadminController::class, 'index'])->name('home');
+Route::get('/superadmin/create-customer', [SuperadminController::class, 'create_customer'])->name('superadmin.create_customer');
+Route::post('/superadmin/store-customer', [SuperadminController::class, 'store_customer'])->name('superadmin.customers.store');
 
 
-//Customer Routes
+// Customer Routes
 
-
-
-//Drugstore Routes
+// Drugstore Routes
